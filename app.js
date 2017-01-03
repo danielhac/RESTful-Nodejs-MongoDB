1e1c33d77654ea29dcb3ad64f4506197607263db
Dan <danhboi23@hotmail.com>
2017-01-03 14:04:08 -0800

    Added and tested bookRouter

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res) {
+        var responseJson = {test: "Testing API"};
+
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
     res.send('Working!');
 });
