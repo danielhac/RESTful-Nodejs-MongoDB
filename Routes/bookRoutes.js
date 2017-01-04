d994cf6866adbc6c871649fd94948cebc7f65508
Dan <danhboi23@hotmail.com>
2017-01-03 19:17:25 -0800

    Wired up Middleware, Put & Patch functions

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -28,14 +28,58 @@ var routes = function(Book) {
             });
         });
 
+    // Middleware for 'id' route - Goes thru here 1st before next section
+    bookRouter.use('/:id', function(req, res, next) {
+        Book.findById(req.params.id, function(err, book) {
+            if (err)
+                res.status(500).send(err);
+            else if (book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('Book not found!')
+            }
+        });
+    });
+
     bookRouter.route('/:id')
         .get(function(req, res) {
-
+            res.json(req.book);
+        })
+        .put(function(req, res) {
             Book.findById(req.params.id, function(err, book) {
+                req.book.title = req.body.title;
+                req.book.author = req.body.author;
+                req.book.genre = req.body.genre;
+                req.book.read = req.body.read;
+
+                req.book.save(function(err) {
+                    if (err)
+                        res.status(500).send(err);
+                    else {
+                        res.json(req.book);
+                    }
+                });
+            });
+        })
+        .patch(function(req, res) {
+            // Delete '_id' to eliminate issues for the Forloop next
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            // Forloop to get any data passed in
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+
+            // Save the change
+            req.book.save(function(err) {
                 if (err)
                     res.status(500).send(err);
-                else
-                    res.json(book);
+                else {
+                    res.json(req.book);
+                }
             });
         });
 
